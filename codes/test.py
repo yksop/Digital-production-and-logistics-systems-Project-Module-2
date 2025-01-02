e0a2bb4e3ebdc781807fba9f2807ece76527714e
Posky <posky2002@gmail.com>
2025-01-02 12:15:28 +0100

    Add distances in multi_vechicle.ipynb

--- codes/test.py
+++ codes/test.py
@@ -9,25 +9,31 @@ load_dotenv()
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
+
 def test_get_location_info(api_key: str) -> None:
     """
     The file locations.txt can be changed and when executed this function, the database in places_id.csv will be updated.
     """
     gli.main(api_key)
 
-def tes_distance_matrix(api_key: str) -> None:
+
+def test_distance_matrix(api_key: str) -> None:
+    # TODO: when locations.txt change, it is not upadating the json file
     """
     This function will create a time matrix in minutes for all the locations in the database.
     Args:
         TARGET_WEEKDAY (int): The target weekday (0=Monday, 6=Sunday).
         TARGET_HOUR (int): The target hour (24-hour format).
         api_key (str): Google Maps API key.
-    
+
     """
     TARGET_WEEKDAY = 0  # Day of the week (0=Monday, 6=Sunday)
     TARGET_HOUR = 8  # Hour of the day (24-hour format)s
-    matrix = dm.main(TARGET_WEEKDAY, TARGET_HOUR, api_key)
-    print(matrix)
+    matrix_time, matrix_distance = dm.main(TARGET_WEEKDAY, TARGET_HOUR, api_key)
+    print(matrix_time)
+    print("\n\n")
+    print(matrix_distance)
+
 
 def test_image_map_markers(api_key: str) -> None:
     """
@@ -40,6 +46,7 @@ def test_image_map_markers(api_key: str) -> None:
     path_loc = os.path.join(current_dir, "data\\locations.txt")
     locations_to_plot = gli.get_location_from_file(path_loc)
     imm.main(locations_to_plot, api_key)
+
 
 def test_get_routes(api_key: str) -> None:
     """
@@ -60,14 +67,14 @@ def test_get_routes(api_key: str) -> None:
     gr.main(origin, destination, n_route, TARGET_WEEKDAY, TARGET_HOUR, api_key)
 
 
-
 def main() -> None:
     api_key = os.getenv("GOOGLE_MAPS_API_KEY")
 
-    # test_get_location_info(api_key)
-    # tes_distance_matrix(api_key)
+    test_get_location_info(api_key) # run this when locations.txt is changed
+    test_distance_matrix(api_key)
     # test_image_map_markers(api_key)
     # test_get_routes(api_key)
+
 
 if __name__ == "__main__":
     main()
